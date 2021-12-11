d20651dd50f43679281f163faa067dffc1c64477
Bruno Raoult <braoult@gmail.com>
2021-12-11 10:35:39 +0100

    typos on C sources day comment

--- 2021/day07/aoc-c.c
+++ 2021/day07/aoc-c.c
@@ -1,4 +1,4 @@
-/* aoc-c: Advent2021 game, day 6 parts 1 & 2
+/* aoc-c: Advent2021 game, day 7 parts 1 & 2
  *
  * Copyright (C) 2021 Bruno Raoult ("br")
  * Licensed under the GNU General Public License v3.0 or later.
