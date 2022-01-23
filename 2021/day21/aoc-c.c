cf2bed124ee69c2acc7a5d3b65e523a7b1567504
Bruno Raoult <braoult@gmail.com>
2022-01-23 21:58:17 +0100

    day 21 init

---
+++ 2021/day21/aoc-c.c
@@ -0,0 +1,62 @@
+/* aoc-c.c: Advent of Code 2021, day 21 parts 1 & 2
+ *
+ * Copyright (C) 2022 Bruno Raoult ("br")
+ * Licensed under the GNU General Public License v3.0 or later.
+ * Some rights reserved. See COPYING.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later <https://spdx.org/licenses/GPL-3.0-or-later.html>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "pool.h"
+#include "debug.h"
+#include "bits.h"
+#include "list.h"
+
+static int part1()
+{
+    return 1;
+}
+static int part2()
+{
+    return 1;
+}
+
+static int usage(char *prg)
+{
+    fprintf(stderr, "Usage: %s [-d debug_level] [-p part]\n", prg);
+    return 1;
+}
+
+int main(int ac, char **av)
+{
+    int opt, part = 1;
+
+    while ((opt = getopt(ac, av, "d:p:")) != -1) {
+        switch (opt) {
+            case 'd':
+                debug_level_set(atoi(optarg));
+                break;
+            case 'p':                             /* 1 or 2 */
+                part = atoi(optarg);
+                if (part < 1 || part > 2)
+                    return usage(*av);
+                break;
+            default:
+                return usage(*av);
+        }
+    }
+    if (optind < ac)
+        return usage(*av);
+
+    printf("%s : res=%d\n", *av, part == 1? part1(): part2());
+
+    exit(0);
+}
