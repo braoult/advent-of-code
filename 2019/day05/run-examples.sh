b206ee5b8743cd88d3d3d14a0aa7e91abbd5e758
Bruno <br@eowyn.lan>
2022-09-24 12:12:12 +0200

    2019 day5: fix run-example.sh reversed results

--- 2019/day05/run-examples.sh
+++ 2019/day05/run-examples.sh
@@ -1,48 +1,50 @@
 #!/usr/bin/env bash
 
+make compile
+
 printf "***** EXAMPLE.txt: input value, then output it\n"
 printf "Expected: 1\t"
 ./aoc-c -i 1 < EXAMPLE.txt
 printf "Expected: 5\t"
 ./aoc-c -i 5 < EXAMPLE.txt
 
-printf "\n\n***** EXAMPLE2.txt: equal test, position mode\n"
+printf "\n***** EXAMPLE2.txt: equal test, position mode\n"
 printf "Expected: 1\t"
 ./aoc-c -i 8 < EXAMPLE2.txt
 printf "Expected: 0\t"
 ./aoc-c -i 0 < EXAMPLE.txt
 
-printf "\n\n***** EXAMPLE3.txt: less than test, position mode\n"
+printf "\n***** EXAMPLE3.txt: less than test, position mode\n"
 printf "Expected: 1\t"
-./aoc-c -i 8 < EXAMPLE3.txt
+./aoc-c -i 7 < EXAMPLE3.txt
 printf "Expected: 0\t"
-./aoc-c -i 0 < EXAMPLE3.txt
+./aoc-c -i 8 < EXAMPLE3.txt
 
-printf "\n\n***** EXAMPLE4.txt: equal test, immediate mode\n"
+printf "\n***** EXAMPLE4.txt: equal test, immediate mode\n"
 printf "Expected: 1\t"
 ./aoc-c -i 8 < EXAMPLE4.txt
 printf "Expected: 0\t"
 ./aoc-c -i 0 < EXAMPLE4.txt
 
-printf "\n\n***** EXAMPLE5.txt: less than test, immediate mode\n"
+printf "\n***** EXAMPLE5.txt: less than test, immediate mode\n"
 printf "Expected: 1\t"
-./aoc-c -i 8 < EXAMPLE5.txt
+./aoc-c -i 7 < EXAMPLE5.txt
 printf "Expected: 0\t"
-./aoc-c -i 0 < EXAMPLE5.txt
+./aoc-c -i 8 < EXAMPLE5.txt
 
-printf "\n\n***** EXAMPLE6.txt: equal/jump test, position mode\n"
+printf "\n***** EXAMPLE6.txt: equal/jump test, position mode\n"
 printf "Expected: 1\t"
 ./aoc-c -i 8 < EXAMPLE6.txt
 printf "Expected: 0\t"
 ./aoc-c -i 0 < EXAMPLE6.txt
 
-printf "\n\n***** EXAMPLE7.txt: equal/jump test, immediate mode\n"
+printf "\n***** EXAMPLE7.txt: equal/jump test, immediate mode\n"
 printf "Expected: 1\t"
 ./aoc-c -i 8 < EXAMPLE7.txt
 printf "Expected: 0\t"
 ./aoc-c -i 0 < EXAMPLE7.txt
 
-printf "\n\n***** EXAMPLE8.txt: equal/less/jump test, mixed mode\n"
+printf "\n***** EXAMPLE8.txt: equal/less/jump test, mixed mode\n"
 printf "Expected:999\t"
 ./aoc-c -i 7 < EXAMPLE8.txt
 printf "Expected: 1000\t"
