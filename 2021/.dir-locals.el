f74a1ffb8ae6d8c85424293a68c7d304bce93371
Bruno Raoult <braoult@gmail.com>
2022-10-05 16:36:57 +0200

    delete .projectile and .dir-locals.el

--- 2021/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((eval . (let ((root (expand-file-name (projectile-project-root))))
-                   (setq-local
-                    flycheck-gcc-include-path (list (concat root "include"))
-                    compile-command (concat "make -C " root " all")))))))
