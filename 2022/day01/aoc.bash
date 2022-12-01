dbff06e5da6df4ba9be00751550bb7f9d932680d
Bruno Raoult <braoult@gmail.com>
2022-12-01 10:28:40 +0100

    2022 day 1: prepare bash common code + bash solutions

---
+++ 2022/day01/aoc.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# aoc.bash: Advent of Code 2022, day 1
+#
+# Copyright (C) 2022 Bruno Raoult ("br")
+# Licensed under the GNU General Public License v3.0 or later.
+# Some rights reserved. See COPYING.
+#
+# You should have received a copy of the GNU General Public License along with this
+# program. If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later <https://spdx.org/licenses/GPL-3.0-or-later.html>
+
+. common.bash
+
+declare -a total
+declare max=0
+declare -i res
+
+parse() {
+    local -i elf=0
+
+    while read -r line; do
+        if [[ -n $line ]]; then
+            (( total[elf] += line))
+        else
+            (( total[elf] > max )) && (( max = total[elf] ))
+            ((elf++))
+        fi
+    done
+}
+
+part1() {
+    res=$max
+}
+
+part2() {
+    local -i i elf newbest
+    res=0
+    for ((i=0; i<3; ++i)); do
+        newbest=0
+        for ((elf=0; elf<${#total[@]}; ++elf)); do
+            if (( total[elf] > total[newbest] )); then
+                newbest=$elf
+            fi
+        done
+        (( res+=total[newbest] ))
+        unset "total[$newbest]"                   # remove current max
+        total=("${total[@]}")                     # pack array
+        #printf "%d: %s\n" "$i" "${total[*]}"
+    done
+}
+
+main "$@"
+printf "%s: res=%s\n" "$cmdname" "$res"
+exit 0
---
+++ 2022/day01/common.bash
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#
+# common.bash: Advent of Code 2022, common bash functions
+#
+# Copyright (C) 2022 Bruno Raoult ("br")
+# Licensed under the GNU General Public License v3.0 or later.
+# Some rights reserved. See COPYING.
+#
+# You should have received a copy of the GNU General Public License along with this
+# program. If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later <https://spdx.org/licenses/GPL-3.0-or-later.html>
+
+# shellcheck disable=2034
+export cmdname=${0##*/}
+export debug=0
+export part=1
+export res=""
+
+shopt -s extglob
+set -o noglob
+
+usage() {
+    printf "usage: %s [-d DEBUG] [-p PART]\n" "$cmdname"
+}
+
+checkargs() {
+    while getopts p:d: todo; do
+        case "$todo" in
+            d)
+                if [[ "$OPTARG" =~ ^[[:digit:]+]$ ]]; then
+                    debug="$OPTARG"
+                else
+                    printf "%s: illegal [%s] debug level.\n" "$CMD" "$OPTARG"
+                    exit 1
+                fi
+                ;;
+            p)
+                if [[ "$OPTARG" =~ ^[12]$ ]]; then
+                    part="$OPTARG"
+                else
+                    printf "%s: illegal [%s] part.\n" "$CMD" "$OPTARG"
+                    exit 1
+                fi
+                ;;
+            *)
+                usage
+                exit 1
+                ;;
+        esac
+    done
+    # Now check remaining argument (backup directory)
+    shift $((OPTIND - 1))
+
+    (( $# > 1 )) && usage
+}
+
+main() {
+    checkargs "$@"
+    parse
+    if ((part == 1)); then
+        part1
+    else
+        part2
+    fi
+}
