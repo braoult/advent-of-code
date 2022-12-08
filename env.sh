64ad068ec80d67346857abb8da0824d337ba3249
Bruno Raoult <braoult@gmail.com>
2022-12-08 12:45:04 +0100

    2022/day 5 init

--- env.sh
+++ env.sh
@@ -5,3 +5,11 @@ export LD_LIBRARY_PATH=../lib
 # usage:
 # TIME command < inputfile
 alias TIME='\time -f "\ttime: %E real, %U user, %S sys\n\tcontext-switch:\t%c+%w, page-faults: %F+%R\n"'
+
+# Get (move) ~/Downloads/Day* file to README.html in current directory, then
+# run "make org"
+org() {
+    mv ~/Downloads/Day* README.html
+    make org
+    rm README.html
+}
